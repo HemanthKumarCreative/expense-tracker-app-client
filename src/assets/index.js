18f89cd01644bb988b75df1c8d41117de7d5f125
Hemanth Kumar <avisihks@gmail.com>
2024-01-10 15:35:54 +0530

    Add prod

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
 const BASE_URL = "https://exp-track-service.onrender.com";
-const CLIENT_URL = "http://localhost:3000";
 const NO_EXPENSE_MESSAGE = "You haven't added your expenses yet";
 const NO_REPORTS_MESSAGE = "You haven't generated the reports yet";
-export { BASE_URL, CLIENT_URL, NO_EXPENSE_MESSAGE, NO_REPORTS_MESSAGE };
+export { BASE_URL, NO_EXPENSE_MESSAGE, NO_REPORTS_MESSAGE };
