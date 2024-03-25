bd5bae33b7ff75a8d63a37e8d962c1969623018a
Hemanth Kumar <avisihks@gmail.com>
2024-03-25 21:32:22 +0530

    update uri

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://exp-track-service.onrender.com";
+const BASE_URL = "https://expense-tracker-app-server.onrender.com";
 const NO_EXPENSE_MESSAGE = "You haven't added your expenses yet";
 const NO_REPORTS_MESSAGE = "You haven't generated the reports yet";
 export { BASE_URL, NO_EXPENSE_MESSAGE, NO_REPORTS_MESSAGE };
